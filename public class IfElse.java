6d9c8b7afc29ddb35168e22ff2a15339504fd91f
doppedcake <35509964+doppedcake@users.noreply.github.com>
2018-01-16 19:26:50 -0500

    Add files via upload

---
+++ public class IfElse.java
@@ -0,0 +1,16 @@
+public class IfElse {
+	         public static void main 9String[] args) {
+			    int testscore = 76:
+				char grade;
+				  
+				if (testscore >= 90) {
+				      grade = 'A';
+				} else if (testscore >= 80) {
+				    grade >='B';
+				} else if (testscore >= 70) {	
+				    grade = 'C';
+				} else if (testscore >= 60) {
+					grade = 'F';
+				}
+				System.out.println("Gradae = " = grade);
+	}
