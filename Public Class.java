5c137cc99668c78b2183c5cc6b5d681e6f45d92b
doppedcake <35509964+doppedcake@users.noreply.github.com>
2018-01-16 19:04:46 -0500

    Add files via upload

---
+++ Public Class.java
@@ -0,0 +1,14 @@
+importt java.util.Scanner;
+
+public class Hello_plus {
+    Public static void main (String [] args) {
+	    System.out.println("Imput a number to add");
+		Scanner scan = new Scanner(System. in);
+		int num1 = scan.nextInt();
+		System.out.[rintln("Pick another number to add");
+		int num2 = scan.nextInt);
+		int num3 = num1 + num2;
+		System.out.println("The answer is" + num3);
+	}
+
+}	
